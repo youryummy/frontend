e7632d5bb6d9e5795eb2d1633ef43046bb86dd6f
Maribel Ramos <62081471+maribelrb22@users.noreply.github.com>
2022-12-19 19:20:18 +0100

    feat: implements menu responsive
    Co-Authored-By: javivm17 <javivm17@users.noreply.github.com>

--- youryummy/pages/_app.js
+++ youryummy/pages/_app.js
@@ -2,12 +2,20 @@ import "../styles/globals.css";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "../store/store";
 import { Provider } from "react-redux";
+import Menu from "./Menu";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <Component {...pageProps} />
+        <Stack direction="row" maxWidth={"100%"}>
+          <Menu></Menu>
+          <Box bgcolor={"#F9FAFA"} height={"100vh"} width={"100%"}>
+            <Component {...pageProps} />
+          </Box>
+        </Stack>
       </PersistGate>
     </Provider>
   );
---
+++ youryummy/pages/recipe-books.js
@@ -0,0 +1,3 @@
+export default function RecipeBooks() {
+  return <div>Recipe books</div>;
+}
