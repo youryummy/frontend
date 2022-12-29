eea9db778488c7f81c46db7c52db34e7768053ed
alesancor1 <alesancor1@alum.us.es>
2022-12-29 23:57:18 +0100

    fix: Made environment variable public
    NEXT_PUBLIC must be added to env vars to be able to use them client side

--- pages/login/api.js
+++ pages/login/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const backendUrl = process.env.BACKEND_URL ?? "http://localhost:8080";
+const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL ?? "http://localhost:8080";
 
 export const validateInput = (input, type, setState, setError) => {
     if (input === "") {
