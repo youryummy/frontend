64b30e2a27eb669717f1a6efa550b2808b8adaa7
alesancor1 <alesancor1@alum.us.es>
2023-01-15 19:05:19 +0100

    fix: Added env variable BACKEND_URL

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 se
 find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#CLIENT_ID_TEMP#$CLIENT_ID#g"
 find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#CLIENT_SECRET_TEMP#$CLIENT_SECRET#g"
 find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#REDIRECT_URI_TEMP#$REDIRECT_URI#g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#BACKEND_URL_TEMP#$BACKEND_URL#g"
 
 echo "Starting Nextjs"
 exec "$@"
